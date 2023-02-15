9f6af52c671e126ccb2e1b3ba25a397e6d7773f8
jcarre <jcarre@tequilarapido.com>
2023-02-15 15:18:26 +0100

    maj doc

--- block-1/block-1.php
+++ block-1/block-1.php
@@ -1,23 +1,35 @@
 <?php
+/**
+ * Documentation : https://www.advancedcustomfields.com/resources/acf_register_block_type/
+ * Tuto exemple : https://capitainewp.io/formations/acf/blocs-acf-gutenberg/
+ */
 
 function block_1_init() {
-	// Check function exists.
+	// Vérifie si la fonction acf_register_block_type existe.
 	if ( function_exists( 'acf_register_block_type' ) ) {
 
-		// Register a testimonial block.
+		/**
+		 * Enregistre le block acf Block 1 ci-dessous.
+		 * - name : nom du block
+		 * - title : titre du block
+		 * - description : description du block
+		 * - category  https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-registration/#category
+		 * - icon  https://developer.wordpress.org/resource/dashicons/#welcome-widgets-menus
+		 * - render_template : chemin vers le fichier php qui contient le rendu du block
+		 * - enqueue_assets : fonction qui permet d'enregistrer des scripts et styles liés au block
+		 */
+
 		acf_register_block_type( array(
 			'name'            => 'block-1',
 			'title'           => __( 'Block 1' ),
 			'description'     => __( 'A custom testimonial block 1.' ),
-			// category list https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-registration/#category
 			'category'        => 'text',
-			// icon list https://developer.wordpress.org/resource/dashicons/#welcome-widgets-menus
 			'icon'            => 'welcome-widgets-menus',
 			'render_template' => plugin_dir_path( __FILE__ ) . 'block-1-view.php',
-			'enqueue_assets'   => function () {
+			'enqueue_assets'  => function () {
 				wp_enqueue_style(
 					'meetup-block-gutenberg',
-					plugin_dir_url(__FILE__) . 'block-1.css',
+					plugin_dir_url( __FILE__ ) . 'block-1.css',
 				);
 			},
 		) );
